047441e2a90580b425d508d0bf8aee42455c0771
tusmasoma <s.k18@icloud.com>
2024-08-19 19:41:35 +0900

    chore: fix

--- microservice-k8s-demo/customer/cmd/main.go
+++ microservice-k8s-demo/customer/cmd/main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"time"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
-}
-
 func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	fmt.Print("Hello World") //nolint:forbidigo // hoge
 }

8fffa9bc5dffddd4b231ca377e7325efee303057
tusmasoma <s.k18@icloud.com>
2024-08-20 14:30:14 +0900

    feat: add catalog usecase

---
+++ microservice-k8s-demo/catalog/usecase/catalog_item.go
@@ -0,0 +1,105 @@
+//go:generate mockgen -source=$GOFILE -package=mock -destination=./mock/$GOFILE
+package usecase
+
+import (
+	"context"
+
+	"github.com/tusmasoma/go-tech-dojo/pkg/log"
+
+	"github.com/tusmasoma/microservice-k8s-demo/catalog/entity"
+	"github.com/tusmasoma/microservice-k8s-demo/catalog/repository"
+)
+
+type CatalogItemUseCase interface {
+	GetCatalogItem(ctx context.Context, id string) (*entity.CatalogItem, error)
+	ListCatalogItems(ctx context.Context) ([]entity.CatalogItem, error)
+	ListCatalogItemsByName(ctx context.Context, name string) ([]entity.CatalogItem, error)
+	ListCatalogItemsByNameContaining(ctx context.Context, name string) ([]entity.CatalogItem, error)
+	CreateCatalogItem(ctx context.Context, name string, price float64) error
+	UpdateCatalogItem(ctx context.Context, id, name string, price float64) error
+	DeleteCatalogItem(ctx context.Context, id string) error
+}
+
+type catalogItemUseCase struct {
+	cr repository.CatalogItemRepository
+}
+
+func NewCatalogItemUseCase(cr repository.CatalogItemRepository) CatalogItemUseCase {
+	return &catalogItemUseCase{
+		cr: cr,
+	}
+}
+
+func (cu *catalogItemUseCase) GetCatalogItem(ctx context.Context, id string) (*entity.CatalogItem, error) {
+	item, err := cu.cr.Get(ctx, id)
+	if err != nil {
+		log.Error("Failed to get catalog item", log.Ferror(err))
+		return nil, err
+	}
+	return item, nil
+}
+
+func (cu *catalogItemUseCase) ListCatalogItems(ctx context.Context) ([]entity.CatalogItem, error) {
+	items, err := cu.cr.List(ctx)
+	if err != nil {
+		log.Error("Failed to list catalog items", log.Ferror(err))
+		return nil, err
+	}
+	return items, nil
+}
+
+func (cu *catalogItemUseCase) ListCatalogItemsByName(ctx context.Context, name string) ([]entity.CatalogItem, error) {
+	items, err := cu.cr.ListByName(ctx, name)
+	if err != nil {
+		log.Error("Failed to list catalog items by name", log.Ferror(err))
+		return nil, err
+	}
+	return items, nil
+}
+
+func (cu *catalogItemUseCase) ListCatalogItemsByNameContaining(ctx context.Context, name string) ([]entity.CatalogItem, error) {
+	items, err := cu.cr.ListByNameContaining(ctx, name)
+	if err != nil {
+		log.Error("Failed to list catalog items by name containing", log.Ferror(err))
+		return nil, err
+	}
+	return items, nil
+}
+
+func (cu *catalogItemUseCase) CreateCatalogItem(ctx context.Context, name string, price float64) error {
+	item, err := entity.NewCatalogItem(name, price)
+	if err != nil {
+		log.Error("Failed to create catalog item", log.Ferror(err))
+		return err
+	}
+	if err = cu.cr.Create(ctx, *item); err != nil {
+		log.Error("Failed to create catalog item", log.Ferror(err))
+		return err
+	}
+	return nil
+}
+
+func (cu *catalogItemUseCase) UpdateCatalogItem(ctx context.Context, id, name string, price float64) error {
+	item, err := cu.cr.Get(ctx, id)
+	if err != nil {
+		log.Error("Failed to get catalog item", log.Ferror(err))
+		return err
+	}
+
+	item.Name = name
+	item.Price = price
+
+	if err = cu.cr.Update(ctx, *item); err != nil {
+		log.Error("Failed to update catalog item", log.Ferror(err))
+		return err
+	}
+	return nil
+}
+
+func (cu *catalogItemUseCase) DeleteCatalogItem(ctx context.Context, id string) error {
+	if err := cu.cr.Delete(ctx, id); err != nil {
+		log.Error("Failed to delete catalog item", log.Ferror(err))
+		return err
+	}
+	return nil
+}
---
+++ microservice-k8s-demo/catalog/usecase/catalog_item_test.go
@@ -0,0 +1,558 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+
+	"github.com/tusmasoma/microservice-k8s-demo/catalog/entity"
+	"github.com/tusmasoma/microservice-k8s-demo/catalog/repository/mock"
+)
+
+func TestUseCase_GetCatalogItem(t *testing.T) {
+	t.Parallel()
+
+	itemID := uuid.New().String()
+
+	item := &entity.CatalogItem{
+		ID:    itemID,
+		Name:  "item",
+		Price: 100,
+	}
+
+	patterns := []struct {
+		name  string
+		setup func(
+			m *mock.MockCatalogItemRepository,
+		)
+		arg struct {
+			ctx context.Context
+			id  string
+		}
+		want struct {
+			item *entity.CatalogItem
+			err  error
+		}
+	}{
+		{
+			name: "success",
+			setup: func(tr *mock.MockCatalogItemRepository) {
+				tr.EXPECT().Get(gomock.Any(), itemID).Return(item, nil)
+			},
+			arg: struct {
+				ctx context.Context
+				id  string
+			}{
+				ctx: context.Background(),
+				id:  itemID,
+			},
+			want: struct {
+				item *entity.CatalogItem
+				err  error
+			}{
+				item: item,
+				err:  nil,
+			},
+		},
+	}
+
+	for _, tt := range patterns {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			tr := mock.NewMockCatalogItemRepository(ctrl)
+
+			if tt.setup != nil {
+				tt.setup(tr)
+			}
+
+			tuc := NewCatalogItemUseCase(tr)
+
+			getCatalogItem, err := tuc.GetCatalogItem(tt.arg.ctx, tt.arg.id)
+
+			if (err != nil) != (tt.want.err != nil) {
+				t.Errorf("GetCatalogItem() error = %v, wantErr %v", err, tt.want.err)
+			} else if err != nil && tt.want.err != nil && err.Error() != tt.want.err.Error() {
+				t.Errorf("GetCatalogItem() error = %v, wantErr %v", err, tt.want.err)
+			}
+
+			if !reflect.DeepEqual(getCatalogItem, tt.want.item) {
+				t.Errorf("GetCatalogItem() got = %v, want %v", getCatalogItem, tt.want.item)
+			}
+		})
+	}
+}
+
+func TestUseCase_ListCatalogItems(t *testing.T) {
+	t.Parallel()
+
+	items := []entity.CatalogItem{
+		{
+			ID:    uuid.New().String(),
+			Name:  "item1",
+			Price: 100,
+		},
+		{
+			ID:    uuid.New().String(),
+			Name:  "item2",
+			Price: 200,
+		},
+	}
+
+	patterns := []struct {
+		name  string
+		setup func(
+			m *mock.MockCatalogItemRepository,
+		)
+		arg struct {
+			ctx context.Context
+		}
+		want struct {
+			items []entity.CatalogItem
+			err   error
+		}
+	}{
+		{
+			name: "success",
+			setup: func(tr *mock.MockCatalogItemRepository) {
+				tr.EXPECT().List(gomock.Any()).Return(items, nil)
+			},
+			arg: struct {
+				ctx context.Context
+			}{
+				ctx: context.Background(),
+			},
+			want: struct {
+				items []entity.CatalogItem
+				err   error
+			}{
+				items: items,
+				err:   nil,
+			},
+		},
+	}
+
+	for _, tt := range patterns {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			tr := mock.NewMockCatalogItemRepository(ctrl)
+
+			if tt.setup != nil {
+				tt.setup(tr)
+			}
+
+			tuc := NewCatalogItemUseCase(tr)
+
+			getCatalogItems, err := tuc.ListCatalogItems(tt.arg.ctx)
+
+			if (err != nil) != (tt.want.err != nil) {
+				t.Errorf("ListCatalogItems() error = %v, wantErr %v", err, tt.want.err)
+			} else if err != nil && tt.want.err != nil && err.Error() != tt.want.err.Error() {
+				t.Errorf("ListCatalogItems() error = %v, wantErr %v", err, tt.want.err)
+			}
+
+			if !reflect.DeepEqual(getCatalogItems, tt.want.items) {
+				t.Errorf("ListCatalogItems() got = %v, want %v", getCatalogItems, tt.want.items)
+			}
+		})
+	}
+}
+
+func TestUseCase_ListCatalogItemsByName(t *testing.T) {
+	t.Parallel()
+
+	item1 := entity.CatalogItem{
+		ID:    uuid.New().String(),
+		Name:  "item1",
+		Price: 100,
+	}
+
+	patterns := []struct {
+		name  string
+		setup func(
+			m *mock.MockCatalogItemRepository,
+		)
+		arg struct {
+			ctx  context.Context
+			name string
+		}
+		want struct {
+			items []entity.CatalogItem
+			err   error
+		}
+	}{
+		{
+			name: "success",
+			setup: func(tr *mock.MockCatalogItemRepository) {
+				tr.EXPECT().ListByName(gomock.Any(), "item1").Return(
+					[]entity.CatalogItem{item1},
+					nil,
+				)
+			},
+			arg: struct {
+				ctx  context.Context
+				name string
+			}{
+				ctx:  context.Background(),
+				name: "item1",
+			},
+			want: struct {
+				items []entity.CatalogItem
+				err   error
+			}{
+				items: []entity.CatalogItem{item1},
+				err:   nil,
+			},
+		},
+	}
+
+	for _, tt := range patterns {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			tr := mock.NewMockCatalogItemRepository(ctrl)
+
+			if tt.setup != nil {
+				tt.setup(tr)
+			}
+
+			tuc := NewCatalogItemUseCase(tr)
+
+			getCatalogItems, err := tuc.ListCatalogItemsByName(tt.arg.ctx, tt.arg.name)
+
+			if (err != nil) != (tt.want.err != nil) {
+				t.Errorf("ListCatalogItemsByName() error = %v, wantErr %v", err, tt.want.err)
+			} else if err != nil && tt.want.err != nil && err.Error() != tt.want.err.Error() {
+				t.Errorf("ListCatalogItemsByName() error = %v, wantErr %v", err, tt.want.err)
+			}
+
+			if !reflect.DeepEqual(getCatalogItems, tt.want.items) {
+				t.Errorf("ListCatalogItemsByName() got = %v, want %v", getCatalogItems, tt.want.items)
+			}
+		})
+	}
+}
+
+func TestUseCase_ListCatalogItemsByNameContaining(t *testing.T) {
+	t.Parallel()
+
+	item1 := entity.CatalogItem{
+		ID:    uuid.New().String(),
+		Name:  "appleTV",
+		Price: 100,
+	}
+	item2 := entity.CatalogItem{
+		ID:    uuid.New().String(),
+		Name:  "new appleTV",
+		Price: 200,
+	}
+
+	patterns := []struct {
+		name  string
+		setup func(
+			m *mock.MockCatalogItemRepository,
+		)
+		arg struct {
+			ctx  context.Context
+			name string
+		}
+		want struct {
+			items []entity.CatalogItem
+			err   error
+		}
+	}{
+		{
+			name: "success",
+			setup: func(tr *mock.MockCatalogItemRepository) {
+				tr.EXPECT().ListByNameContaining(gomock.Any(), "apple").Return(
+					[]entity.CatalogItem{
+						item1,
+						item2,
+					},
+					nil,
+				)
+			},
+			arg: struct {
+				ctx  context.Context
+				name string
+			}{
+				ctx:  context.Background(),
+				name: "apple",
+			},
+			want: struct {
+				items []entity.CatalogItem
+				err   error
+			}{
+				items: []entity.CatalogItem{
+					item1,
+					item2,
+				},
+				err: nil,
+			},
+		},
+		{
+			name: "success: no items",
+			setup: func(tr *mock.MockCatalogItemRepository) {
+				tr.EXPECT().ListByNameContaining(gomock.Any(), "docomo").Return(
+					[]entity.CatalogItem{},
+					nil,
+				)
+			},
+			arg: struct {
+				ctx  context.Context
+				name string
+			}{
+				ctx:  context.Background(),
+				name: "docomo",
+			},
+			want: struct {
+				items []entity.CatalogItem
+				err   error
+			}{
+				items: []entity.CatalogItem{},
+				err:   nil,
+			},
+		},
+	}
+
+	for _, tt := range patterns {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			tr := mock.NewMockCatalogItemRepository(ctrl)
+
+			if tt.setup != nil {
+				tt.setup(tr)
+			}
+
+			tuc := NewCatalogItemUseCase(tr)
+
+			getCatalogItems, err := tuc.ListCatalogItemsByNameContaining(tt.arg.ctx, tt.arg.name)
+
+			if (err != nil) != (tt.want.err != nil) {
+				t.Errorf("ListCatalogItemsByNameContaining() error = %v, wantErr %v", err, tt.want.err)
+			} else if err != nil && tt.want.err != nil && err.Error() != tt.want.err.Error() {
+				t.Errorf("ListCatalogItemsByNameContaining() error = %v, wantErr %v", err, tt.want.err)
+			}
+
+			if !reflect.DeepEqual(getCatalogItems, tt.want.items) {
+				t.Errorf("ListCatalogItemsByNameContaining() got = %v, want %v", getCatalogItems, tt.want.items)
+			}
+		})
+	}
+}
+
+func TestUseCase_CreateCatalogItem(t *testing.T) {
+	t.Parallel()
+
+	patterns := []struct {
+		name  string
+		setup func(
+			m *mock.MockCatalogItemRepository,
+		)
+		arg struct {
+			ctx   context.Context
+			name  string
+			price float64
+		}
+		wantErr error
+	}{
+		{
+			name: "success",
+			setup: func(tr *mock.MockCatalogItemRepository) {
+				tr.EXPECT().Create(
+					gomock.Any(),
+					gomock.Any(),
+				).Do(func(_ context.Context, item entity.CatalogItem) {
+					if item.Name != "item" {
+						t.Errorf("unexpected Name: got %v, want %v", item.Name, "item")
+					}
+					if item.Price != 100 {
+						t.Errorf("unexpected Price: got %v, want %v", item.Price, 100)
+					}
+				}).Return(nil)
+			},
+			arg: struct {
+				ctx   context.Context
+				name  string
+				price float64
+			}{
+				ctx:   context.Background(),
+				name:  "item",
+				price: 100,
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range patterns {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			tr := mock.NewMockCatalogItemRepository(ctrl)
+
+			if tt.setup != nil {
+				tt.setup(tr)
+			}
+
+			tuc := NewCatalogItemUseCase(tr)
+
+			err := tuc.CreateCatalogItem(tt.arg.ctx, tt.arg.name, tt.arg.price)
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("want: %v, got: %v", tt.wantErr, err)
+			}
+		})
+	}
+}
+
+func TestUseCase_UpdateCatalogItem(t *testing.T) {
+	t.Parallel()
+
+	itemID := uuid.New().String()
+
+	item := &entity.CatalogItem{
+		ID:    itemID,
+		Name:  "item",
+		Price: 100,
+	}
+
+	patterns := []struct {
+		name  string
+		setup func(
+			m *mock.MockCatalogItemRepository,
+		)
+		arg struct {
+			ctx   context.Context
+			id    string
+			name  string
+			price float64
+		}
+		wantErr error
+	}{
+		{
+			name: "success",
+			setup: func(tr *mock.MockCatalogItemRepository) {
+				tr.EXPECT().Get(
+					gomock.Any(),
+					itemID,
+				).Return(item, nil)
+				tr.EXPECT().Update(
+					gomock.Any(),
+					gomock.Any(),
+				).Do(func(_ context.Context, item entity.CatalogItem) {
+					if item.Name != "updated item" {
+						t.Errorf("unexpected Name: got %v, want %v", item.Name, "updated item")
+					}
+					if item.Price != 200 {
+						t.Errorf("unexpected Price: got %v, want %v", item.Price, 200)
+					}
+				}).Return(nil)
+			},
+			arg: struct {
+				ctx   context.Context
+				id    string
+				name  string
+				price float64
+			}{
+				ctx:   context.Background(),
+				id:    itemID,
+				name:  "updated item",
+				price: 200,
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range patterns {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			tr := mock.NewMockCatalogItemRepository(ctrl)
+
+			if tt.setup != nil {
+				tt.setup(tr)
+			}
+
+			tuc := NewCatalogItemUseCase(tr)
+
+			err := tuc.UpdateCatalogItem(tt.arg.ctx, tt.arg.id, tt.arg.name, tt.arg.price)
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("want: %v, got: %v", tt.wantErr, err)
+			}
+		})
+	}
+}
+
+func TestUsaCase_DeleteCatalogItem(t *testing.T) {
+	t.Parallel()
+
+	itemID := uuid.New().String()
+
+	patterns := []struct {
+		name  string
+		setup func(
+			m *mock.MockCatalogItemRepository,
+		)
+		arg struct {
+			ctx context.Context
+			id  string
+		}
+		wantErr error
+	}{
+		{
+			name: "success",
+			setup: func(tr *mock.MockCatalogItemRepository) {
+				tr.EXPECT().Delete(gomock.Any(), itemID).Return(nil)
+			},
+			arg: struct {
+				ctx context.Context
+				id  string
+			}{
+				ctx: context.Background(),
+				id:  itemID,
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range patterns {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			tr := mock.NewMockCatalogItemRepository(ctrl)
+
+			if tt.setup != nil {
+				tt.setup(tr)
+			}
+
+			tuc := NewCatalogItemUseCase(tr)
+
+			err := tuc.DeleteCatalogItem(tt.arg.ctx, tt.arg.id)
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("want: %v, got: %v", tt.wantErr, err)
+			}
+		})
+	}
+}

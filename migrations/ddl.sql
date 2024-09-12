e0056b0639ab4d4ad69747f03e85dc5d628254a5
tusmasoma <s.k18@icloud.com>
2024-09-12 20:22:41 +0900

    chore: fix init

--- migrations/ddl.sql
+++ migrations/ddl.sql
@@ -1,6 +1,9 @@
 CREATE DATABASE IF NOT EXISTS `microservice-k8s-demo-db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `microservice-k8s-demo-db`;
 
+GRANT ALL PRIVILEGES ON `microservice-k8s-demo-db`.* TO 'microservice-k8s-demo'@'%';
+FLUSH PRIVILEGES;
+
 DROP TABLE IF EXISTS CatalogItems;
 DROP TABLE IF EXISTS Customers;
 DROP TABLE IF EXISTS OrderLines;

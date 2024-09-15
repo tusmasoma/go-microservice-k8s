4df3d07b69954ba1e581df63934bdbb5e7475e3f
tusmasoma <s.k18@icloud.com>
2024-09-15 21:42:03 +0900

    chore: add compose

---
+++ migrations/init.d/0_initialize.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS `microservice-k8s-demo-db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- USE `microservice-k8s-demo-db`;
+-- GRANT ALL PRIVILEGES ON `microservice-k8s-demo-db`.* TO 'microservice-k8s-demo'@'%';
+-- FLUSH PRIVILEGES;
--- migrations/init.d/1_create_table.sql
+++ migrations/init.d/1_create_table.sql
@@ -1,8 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `microservice-k8s-demo-db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `microservice-k8s-demo-db`;
-
-GRANT ALL PRIVILEGES ON `microservice-k8s-demo-db`.* TO 'microservice-k8s-demo'@'%';
-FLUSH PRIVILEGES;
 
 DROP TABLE IF EXISTS CatalogItems;
 DROP TABLE IF EXISTS Customers;

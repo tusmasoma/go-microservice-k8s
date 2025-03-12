71b7e09a13b9c497c2a61cc1294aed46f414c5e7
tusmasoma <s.k18@icloud.com>
2025-03-12 19:30:18 +0900

    chore: fix path

--- services/gateway/v1/cmd/main.go
+++ services/gateway/v1/cmd/main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/tusmasoma/go-microservice-k8s/pkg/config"
-	"github.com/tusmasoma/go-microservice-k8s/services/gateway/web"
+	"github.com/tusmasoma/go-microservice-k8s/services/gateway/v1/web"
 	"github.com/tusmasoma/go-tech-dojo/pkg/log"
 	"google.golang.org/grpc"
 
